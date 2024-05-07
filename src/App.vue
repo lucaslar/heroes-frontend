dc3a35bd2e3c9716224a71ab14f4286efec6aa22
Lucas Larisch <lucas.larischgooglemail.com>
2024-05-07 02:51:15 +0200

    Created example components.

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/heroes">Heroes (Composition API)</RouterLink>
+        <RouterLink to="/heroes-oa">Heroes (Options API)</RouterLink>
       </nav>
     </div>
   </header>
