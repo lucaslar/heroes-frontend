c3ba3f962949f6a74dd53dcb670638ed210bb785
Lucas Larisch <lucas.larischgooglemail.com>
2024-05-07 03:00:13 +0200

    Commented out Bootstrap integration.

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/js/bootstrap.min.js'
 
 const app = createApp(App)
 
